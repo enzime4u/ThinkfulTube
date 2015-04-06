8ab4ac30134974c3c5a84d37187ec480e6dbf7d9
Liviu <enzime4u@yahoo.com>
2015-04-06 14:15:41 +0300

    js file

--- js/app.js
+++ js/app.js
@@ -1,5 +1,32 @@
 $(document).ready(function() {
-    $.getJSON('https://www.googleapis.com/youtube/v3/search', function(data){
-    	console.log(data);
-    })
+	$('#search-term').submit(function(event) {
+		event.preventDefault();
+		var searchTerm = $('#query').val();
+		/* get request function */
+		getRequest();
+		console.log($('#query').val());	
+	})
+
 });
+
+	function getRequest(searchTerm) {
+		var params = {
+			q: searchTerm,
+			r: 'json',
+			part: 'snippet',
+			key: 'AIzaSyAZBq6-_H_TPB2YlfcrEHh7kPv-GwEP3Is'
+		};
+		url = 'https://www.googleapis.com/youtube/v3/search';
+		$.getJSON(url, params, function(data){
+			showResults(data.Search);
+		});
+	}
+
+	function showResults(results) {
+		var html = " ";
+		$.each(results, function(index, value) {
+			html += '<p>' + value.Title + '</p>';
+		});
+		$('#search-results').html(html);
+	}
+
